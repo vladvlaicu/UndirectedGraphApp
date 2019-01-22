84f59971d7e45828ffb285ebf88d3db57f49e1f0
Andrei <andrei.matei2010@gmail.com>
2019-01-22 12:48:28 +0200

    Merging Graph initialization and Basic Methods

--- src/models/UndirectedGraph.java
+++ src/models/UndirectedGraph.java
@@ -5,8 +5,14 @@
  */
 package models;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
 import utils.Constants;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  *
@@ -16,12 +22,80 @@ public class UndirectedGraph {
     
     private int numEdges;
     private int numNodes;
-    private List<Edge> edgesList;
+    private List<Edge> edgesList = Arrays.asList();
     private byte[][] adjacencyMatrix;
     private int[] nodesGrades;
     
-    public UndirectedGraph() {
+   public UndirectedGraph(){
+   
+   }
+    
+    public static UndirectedGraph getInitGraph(){
+        return initGraph();
+    }
+
+    public int getnumEdges() {
+        return numEdges;
+    }
+
+    public void setnumEdges(int numEdges) {
+        this.numEdges = numEdges;
+    }
+
+    public int getnumNodes() {
+        return numNodes;
+    }
+
+    public void setnumNodes(int numNodes) {
+        this.numNodes = numNodes;
+    }
+
+    public List<Edge> getList() {
+        return edgesList;
+    }
+
+    public void setList(List<Edge> edgesList) {
+        this.edgesList = edgesList;
+    }
+    
+    public static UndirectedGraph initGraph(){
+        Path pathEdge = Paths.get("./GraphData/GraphDataEdges.txt");
+        Path pathNumNode = Paths.get("./GraphData/GraphDataNrNodes.txt");
+        UndirectedGraph graph = new UndirectedGraph();
+        try {
+            int  nrNodes = Integer.parseInt(Files.readAllLines(pathNumNode).get(0));
+            graph.setnumNodes(nrNodes);
+            initEdges(pathEdge);
+            int nrEdges =(int) Files.lines(pathEdge)
+                               .count();
+            graph.setnumEdges(nrEdges);
+            graph.setList(UndirectedGraph.initEdges(pathEdge));
+        } catch (IOException | IndexOutOfBoundsException ex) {
+            ex.printStackTrace();
+        }
+        return graph;
+    }
+
+    private static List<Edge> initEdges(Path pathEdge) throws IOException {
+        List<Edge> edgesList = Files.lines(pathEdge)
+                                .map(s -> toEdge(s))
+                                .collect(Collectors.toList());
+         return edgesList;
+                                
+    }
+    
+    private static Edge toEdge(String line){
         
+        String vars[] = line.split("\\s+");
+        return new Edge(Integer.parseInt(vars[0]),Integer.parseInt(vars[1]));
+
+
+    }
+    
+    @Override
+    public String toString(){
+        String string = ""+this.numNodes+" "+this.numEdges+" "+this.edgesList;
+        return string;
     }
     
     public void SetAdjacencyMatrix() {
@@ -53,7 +127,7 @@ public class UndirectedGraph {
     
     public int[] GetNodesWithMaxGrade() {
         int[] nodes = new int[Constants.NUM_MAX_NODES];
-        
+        return nodes;
     }
     
     public int[] GetAdjacentNodesForNode(int node) {
