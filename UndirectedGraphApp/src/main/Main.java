2a8b541993df448405ce1991297e209c5783ed70
vladvlaicu <32589610+vladvlaicu@users.noreply.github.com>
2019-01-23 06:07:59 +0200

    Add files via upload
    first cut

---
+++ UndirectedGraphApp/src/main/Main.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+
+import view.MenuFrame;
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import models.UndirectedGraph;
+
+
+
+/**
+ *
+ * @author elev
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MenuFrame menuFrame = new MenuFrame();
+        
+            
+
+    
+    }
+}
